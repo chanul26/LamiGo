93daff520fc23b87963ec3e43d5ada2c1bacd732
Chanul Nanvidu <chanulrcp26@gmail.com>
2026-02-26 23:25:20 +0530

    time changed

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -14,7 +14,7 @@ export function ThemeToggle() {
       <motion.div
         initial={false}
         animate={{ rotate: theme === 'dark' ? 180 : 0 }}
-        transition={{ duration: 0.3, ease: "easeInOut" }}
+        transition={{ duration: 0.4, ease: "easeInOut" }}
       >
         {theme === 'dark' ? <Moon className="w-5 h-5" /> : <Sun className="w-5 h-5" />}
       </motion.div>
